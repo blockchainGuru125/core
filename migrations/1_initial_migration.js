ead91d78b093594250484d2f24d57e9ec9cbbc1a
heswap <heswap@protonmail.com>
2021-06-11 15:50:18 +0800

    test version

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,9 @@
 const HeswapFactory = artifacts.require("HeswapFactory");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(HeswapFactory, '0xdBE55A0daDc80EF88e884f15CE41c26c0Af933a0');
+  await deployer.deploy(HeswapFactory, '0x5dE4C680d3e306eBbd94b5795905f8234B22D803');
   let instance = await HeswapFactory.deployed();
-  await instance.setFeeTo("0x3ea3016aa9e3faaD871AA45232055a424b00bFC1");
+  await instance.setFeeTo("0x5dE4C680d3e306eBbd94b5795905f8234B22D803");
   let res = await instance.feeTo.call();
   console.log('fee - ', res)
 
